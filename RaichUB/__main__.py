c0999eb03cc8ae82b36a667b7107bb2d0c86b633
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-02-04 07:43:35 +0530

    Update __main__.py

--- RaichUB/__main__.py
+++ RaichUB/__main__.py
@@ -1,1 +1,78 @@
+import sys
 
+import ub
+from RaichUB import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
+
+from .Config import Config
+from .core.logger import logging
+from .core.session import raichub
+from .utils import (
+    add_bot_to_logger_group,
+    ipchange,
+    load_plugins,
+    setup_bot,
+    startupmessage,
+    verifyLoggerGroup,
+)
+
+LOGS = logging.getLogger("RaichUB")
+
+print(userbot.__copyright__)
+print("Licensed under the terms of the " + userbot.__license__)
+
+cmdhr = Config.COMMAND_HAND_LER
+
+try:
+    LOGS.info("Starting Userbot")
+    raichub.loop.run_until_complete(setup_bot())
+    LOGS.info("TG Bot Startup Completed")
+except Exception as e:
+    LOGS.error(f"{e}")
+    sys.exit()
+
+
+class RaichuCheck:
+    def __init__(self):
+        self.sucess = True
+
+
+RaichuCheck = RaichuCheck () 
+
+
+async def startup_process():
+    check = await ipchange()
+    if check is not None:
+        RaichuCheck.sucess = False
+        return
+    await verifyLoggerGroup()
+    await load_plugins("plugins")
+    await load_plugins("assistant")
+    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+    print("Yay your userbot is officially working.!!!")
+    print(
+        f"Congratulation, now type {cmdhr}alive to see message if Raichu is live\
+        \nIf you need assistance, head to https://t.me/RaichuOfficial"
+    )
+    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+    await verifyLoggerGroup()
+    await add_bot_to_logger_group(BOTLOG_CHATID)
+    if PM_LOGGER_GROUP_ID != -100:
+        await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
+    await startupmessage()
+    RaichuCheck.sucess = True
+    return
+
+
+raichub.loop.run_until_complete(startup_process())
+
+
+if len(sys.argv) not in (1, 3, 4):
+    raichub.disconnect()
+elif not RaichuCheck.sucess:
+    if HEROKU_APP is not None:
+        HEROKU_APP.restart()
+else:
+    try:
+        raichub.run_until_disconnected()
+    except ConnectionError:
+        pass
